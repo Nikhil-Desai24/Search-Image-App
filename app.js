41aebc4551950b6012ce977bf77822baec7f6273
Nikhil-Desai24 <nikhildesaind024@gmail.com>
2024-01-14 19:56:47 +0530

    Image Application

---
+++ app.js
@@ -0,0 +1,61 @@
+const accessKey = 'lx_rGsp5xvTXuOCPcq1ItMLtpd5rTZxT7nekjRyyDak';
+
+const formE1 = document.querySelector('form');
+
+const inputElement = document.getElementById('search-input');
+
+const searchResult = document.querySelector('.search-results');
+
+const showMore = document.querySelector('#show-more-button')
+
+let inputdata = '';
+let page = 1;
+
+async function searchImages (){
+    inputdata = inputElement.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputdata}&client_id=${accessKey}`
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    const results =  data.results;
+
+    if(page === 1)
+    {
+        searchResult.innerHTML='';
+    }
+
+    results.map((result)=>{
+        const imageWrapper = document.createElement('div');
+        imageWrapper.classList.add('search-result');
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement('a');
+        imageLink.href = result.links.html;
+        imageLink.target = '_blank';
+        imageLink.textContent = result.alt_description;
+
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResult.append(imageWrapper);
+    });
+    page++;
+    if(page > 1)
+    {
+        showMore.style.display = 'block';
+    }
+
+}
+
+formE1.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+
+showMore.addEventListener('click', ()=>{
+    searchImages();
+})
